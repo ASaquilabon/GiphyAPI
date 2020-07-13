31eac4ccda22e985dd04f5f9b27ff62492f169dc
Your name here <asaquilabon@gmail.com>
2020-07-13 12:36:55 -0700

    API now working. Added some css

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
-//Sign up for API KEY
 const apiKey = 'A3r4HTxtapOQo3PBi871U13Twv4g7yie';
 let userSearch = ""
 
-
+$(document).ready(() => {
+    $("#submit").click(()=>{
+        //taking value of the user input
+        userSearch = $("#userSearch").val()
+        //empty out gif-body 
+        $(".gif-body").empty()
+        //get request for giphy
+        var xhr = $.get(`https://api.giphy.com/v1/gifs/search?q=${userSearch}&api_key=${apiKey}&limit=30`);
+        xhr.done((res) => {
+            const gifs = res.data
+            //map through data and pull out image url 
+            console.log(userSearch)
+            gifs.map((gif) => {
+                const imageMedium = gif.images.downsized_medium.url
+                $(".gif-body").append('<img src=' + imageMedium + '">"')
+            })
+        })
+    })
+});
